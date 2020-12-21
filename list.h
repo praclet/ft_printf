3c0a6a6b3e88fdabe2dcf718e557675c3229c070
Philippe Raclet <praclet@student.42lyon.fr>
2020-12-21 16:47:23 +0100

    Saving files

--- list.h
+++ list.h
@@ -6,12 +6,14 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 11:02:48 by praclet           #+#    #+#             */
-/*   Updated: 2020/12/21 10:35:03 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2020/12/21 16:45:08 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIST_H
 # define LIST_H
+
+# include <wchar.h>
 
 # define FLAG_SHARP		1
 # define FLAG_ZERO		2
@@ -28,22 +30,39 @@
 
 # define CONVERSION		"cspdiuxXnfge%"
 
-typedef struct		s_chain {
-	char			*str;
-	void			*arg1;
-	void			*arg2;
-	unsigned char	flags;
-	int				width;
-	int				precision;
-	unsigned char	modifiers;
-	char			conversion;
-	struct s_chain	*next;
-}					t_chain;
+typedef struct					s_chain {
+	char						*str;
+	union
+	{
+		char					arg_char;
+		char					*arg_pchar;
+		wchar_t					*arg_wchar_t;
+		signed char				arg_schar;
+		unsigned char			arg_uchar;
+		double					arg_double;
+		void					*arg_ptr;
+		int						arg_int;
+		unsigned int			arg_uint;
+		long int				arg_lint;
+		short int				arg_short_int;
+		unsigned short int		arg_u_short_int;
+		unsigned long int		arg_ulint;
+		long long int			arg_llint;
+		unsigned long long int	arg_ullint;
+		wint_t					arg_wint_t;
+	}							u_arg;
+	unsigned char				flags;
+	int							width;
+	int							precision;
+	unsigned char				modifiers;
+	char						conversion;
+	struct s_chain				*next;
+}								t_chain;
 
-t_chain				*new_elem(void);
-t_chain				*check_list(t_chain *list);
-void				delete_elem(t_chain *elem);
-void				delete_list(t_chain *list);
-char				*list_finish(t_chain *list);
+t_chain							*new_elem(void);
+t_chain							*check_list(t_chain *list);
+void							delete_elem(t_chain *elem);
+void							delete_list(t_chain *list);
+char							*list_finish(t_chain *list);
 
 #endif
