c9b6f1f5ae7a9a4bf570872151f5fb7132228531
Philippe Raclet <praclet@student.42lyon.fr>
2020-12-23 13:17:41 +0100

    Saving files

--- arg_affectation.c
+++ arg_affectation.c
@@ -6,7 +6,7 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 12:51:58 by praclet           #+#    #+#             */
-/*   Updated: 2020/12/23 13:12:27 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2020/12/23 13:17:29 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,44 @@
 #include "list.h"
 #include "arg_affectation.h"
 
-void	affectation(t_chain *list, va_list ap)
+static void	affectation_di(t_chain *list, va_list ap)
+{
+	if (!list->modifiers)
+		list->u_arg.arg_llint = (int)va_arg(ap, int);
+	if (list->modifiers & MODIFIER_L)
+		list->u_arg.arg_llint = (long int)va_arg(ap, long int);
+	if (list->modifiers & MODIFIER_LL)
+		list->u_arg.arg_llint = (long long int)va_arg(ap, long long int);
+	if (list->modifiers & MODIFIER_H)
+		list->u_arg.arg_llint = (short int)va_arg(ap, int);
+	if (list->modifiers & MODIFIER_HH)
+		list->u_arg.arg_llint = (signed char)va_arg(ap, int);
+}
+
+static void	affectation_uoxx(t_chain *list, va_list ap)
+{
+	if (!list->modifiers)
+		list->u_arg.arg_ullint = (unsigned int)va_arg(ap, unsigned int);
+	if (list->modifiers & MODIFIER_L)
+		list->u_arg.arg_ullint =
+			(unsigned long int)va_arg(ap, unsigned long int);
+	if (list->modifiers & MODIFIER_LL)
+		list->u_arg.arg_ullint =
+			(unsigned long long int)va_arg(ap, unsigned long long int);
+	if (list->modifiers & MODIFIER_H)
+		list->u_arg.arg_ullint = (unsigned short int)va_arg(ap, int);
+	if (list->modifiers & MODIFIER_HH)
+		list->u_arg.arg_ullint = (unsigned char)va_arg(ap, int);
+}
+
+void		affectation(t_chain *list, va_list ap)
 {
 	while (list)
 	{
 		if (ft_strchr("di", list->conversion))
-		{
-			if (!list->modifiers)
-				list->u_arg.arg_llint = (int)va_arg(ap, int);
-			if (list->modifiers & MODIFIER_L)
-				list->u_arg.arg_llint = (long int)va_arg(ap, long int);
-			if (list->modifiers & MODIFIER_LL)
-				list->u_arg.arg_llint =
-					(long long int)va_arg(ap, long long int);
-			if (list->modifiers & MODIFIER_H)
-				list->u_arg.arg_llint = (short int)va_arg(ap, int);
-			if (list->modifiers & MODIFIER_HH)
-				list->u_arg.arg_llint = (signed char)va_arg(ap, int);
-		}
+			affectation_di(list, ap);
 		if (ft_strchr("uoxX", list->conversion))
-		{
-			if (!list->modifiers)
-				list->u_arg.arg_ullint = (unsigned int)va_arg(ap, unsigned int);
-			if (list->modifiers & MODIFIER_L)
-				list->u_arg.arg_ullint =
-					(unsigned long int)va_arg(ap, unsigned long int);
-			if (list->modifiers & MODIFIER_LL)
-				list->u_arg.arg_ullint =
-					(unsigned long long int)va_arg(ap, unsigned long long int);
-			if (list->modifiers & MODIFIER_H)
-				list->u_arg.arg_ullint = (unsigned short int)va_arg(ap, int);
-			if (list->modifiers & MODIFIER_HH)
-				list->u_arg.arg_ullint = (unsigned char)va_arg(ap, int);
-		}
+			affectation_uoxx(list, ap);
 		if (ft_strchr("c", list->conversion))
 		{
 			if (!list->modifiers)
